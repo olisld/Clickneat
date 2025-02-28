2c46c1feccf81fa529ca872553f6ab18aac74a6d
olisld <olivier.soulard4@gmail.com>
2025-02-28 10:57:45 +0100

    last commit

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\CategorieController;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('restaurants.index');
 });
 
 // Routepour les restaurants
